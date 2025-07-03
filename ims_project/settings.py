d6b880d82fd2b47eb83b34b095ce90f0f8ff7cd3
QBITX Solutions <qbitx@Shaikats-Mac-mini.local>
2025-07-03 17:35:55 +0600

    4th commit

--- ims_project/settings.py
+++ ims_project/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+FORCE_SCRIPT_NAME = '/imstransform'
 STATIC_URL = '/imstransform/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
