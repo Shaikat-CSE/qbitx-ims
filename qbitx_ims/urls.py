70bb9eff41189a76018828e2cb9a358396d0e31d
shaikat-cse <shaikat143@gmail.com>
2025-07-06 15:11:58 +0600

    updates urls

--- qbitx_ims/urls.py
+++ qbitx_ims/urls.py
@@ -21,10 +21,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('inventory.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('imstransform/admin/', admin.site.urls),
+    path('imstransform/', include('inventory.urls')),
+    path('imstransform/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('imstransform/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
 
 if settings.DEBUG:
